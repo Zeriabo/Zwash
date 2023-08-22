63848efcd1ff71b1828405c8c0feefb9dd2ea117
Zeriabo <zeriab@hotmail.com>
2023-08-22 12:12:48 +0300

    Create RegisterCarScreen.tsx

---
+++ src/screens/RegisterCarScreen.tsx
@@ -0,0 +1,88 @@
+import React, {useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {Card, TextInput, Button} from 'react-native-paper';
+import YearPicker from 'react-native-month-year-picker';
+import {useDispatch, useSelector} from 'react-redux';
+import {registerCar} from '../redux/actions/carActions';
+import Car from '../redux/types/CarType';
+
+const CarRegistrationForm = () => {
+  const [registrationPlate, setRegistrationPlate] = useState('');
+  const [manufacture, setManufacture] = useState('');
+  const [dateOfManufacture, setDateOfManufacture] = useState(new Date());
+  const [isDatePickerVisible, setDatePickerVisible] = useState(false);
+  const token = useSelector((state: any) => state.user.user.token);
+  const dispatch = useDispatch();
+  const showDatePicker = () => {
+    setDatePickerVisible(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisible(false);
+  };
+
+  const handleDateChange = (event, selectedDate) => {
+    if (selectedDate !== undefined) {
+      setDateOfManufacture(selectedDate);
+    }
+    hideDatePicker();
+  };
+
+  const handleRegisterCar = () => {
+    const car: Car = {
+      registerationPlate: registrationPlate,
+      manufacture: manufacture,
+      dateOfManufacture: dateOfManufacture,
+      token: token,
+      deviceRegistrationToken: '',
+    };
+    dispatch(registerCar(car));
+  };
+
+  return (
+    <View style={styles.container}>
+      <Card style={styles.card}>
+        <Card.Title title="Register a Car" />
+        <Card.Content>
+          <TextInput
+            label="Registration Plate"
+            value={registrationPlate}
+            onChangeText={text => setRegistrationPlate(text)}
+          />
+          <TextInput
+            label="Manufacture"
+            value={manufacture}
+            onChangeText={text => setManufacture(text)}
+          />
+          <Button onPress={showDatePicker}>Select Date of Manufacture</Button>
+          {isDatePickerVisible && (
+            <YearPicker value={dateOfManufacture} onChange={handleDateChange} />
+          )}
+        </Card.Content>
+        <Card.Actions style={styles.cardActions}>
+          <Button mode="contained" onPress={handleRegisterCar}>
+            Register Car
+          </Button>
+        </Card.Actions>
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  card: {
+    elevation: 4,
+  },
+  cardActions: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 16,
+  },
+});
+
+export default CarRegistrationForm;
