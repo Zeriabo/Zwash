e1d9d7e1c2ef92e9782e49f496c5ffacb2f3e7b9
Zeriabo <zeriab@hotmail.com>
2023-08-25 11:00:21 +0300

    Update WashesScreen.tsx

--- src/screens/WashesScreen.tsx
+++ src/screens/WashesScreen.tsx
@@ -1,7 +1,95 @@
 import React from 'react';
-
-function WashesScreen() {
-  return <div>WashesScreen</div>;
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {CarWashingProgram, Station} from '../redux/types/stationsActionTypes';
+import {NavigationProp, RouteProp} from '@react-navigation/native';
+import {HeaderBackButton} from '@react-navigation/elements';
+interface Props {
+  route: RouteProp<{params: {station: Station}}, 'params'>;
+  navigation: NavigationProp<any>;
 }
+
+const WashesScreen: React.FC<Props> = ({route, navigation}) => {
+  const {station} = route.params;
+
+  const programs: CarWashingProgram[] = station.programs;
+
+  const handleProgramSelection = (selectedProgram: CarWashingProgram) => {
+    navigation.navigate('Buywash', {selectedProgram});
+  };
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.backButton}>
+          <Text style={styles.backButtonText}>Back</Text>
+        </TouchableOpacity>
+      ),
+      headerTitle: '', // Remove the header title
+      headerTitleAlign: 'left', // Align the header title to the left
+    });
+  }, [navigation]);
+  return (
+    <View style={styles.container}>
+      <HeaderBackButton
+        style={styles.backButton}
+        onPress={() => navigation.goBack()}
+      />
+      <Text style={styles.title}>MyWashes</Text>
+      <Text style={styles.infoText}>Station Name: {station.name}</Text>
+      <Text style={styles.infoText}>Station Address: {station.address}</Text>
+      <Text style={styles.programsTitle}>Programs:</Text>
+      {programs.map(program => (
+        <TouchableOpacity
+          key={program.id}
+          style={styles.programContainer}
+          onPress={() => handleProgramSelection(program)}>
+          <Text style={styles.programText}>{program.program}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  infoText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  programsTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginTop: 20,
+  },
+  programContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    backgroundColor: '#eaeaea',
+    marginBottom: 10,
+  },
+  programText: {
+    fontSize: 14,
+  },
+  backButton: {
+    marginRight: 300,
+  },
+  backButtonText: {
+    fontSize: 16,
+    color: 'blue',
+  },
+});
 
 export default WashesScreen;
